70a6f2026442bb529615d419761f90ed374e6b45
aaaol <28624574+aaaol@users.noreply.github.com>
2025-04-02 11:42:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,12 @@
 
 # 修改默认名称
 sed -i 's/LEDE/H29K/g' package/base-files/files/bin/config_generate
+
+# 修改 WiFi 名称（SSID）从 LEDE → H29K
+sed -i 's/set wireless.default_radio${devidx}.ssid=LEDE/set wireless.default_radio${devidx}.ssid=H29K/g' \
+    package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改加密方式（none → psk2）并添加密码（1234567890）
+sed -i 's/set wireless.default_radio${devidx}.encryption=none/set wireless.default_radio${devidx}.encryption=psk2\\n\\t\\t\\t set wireless.default_radio${devidx}.key=1234567890/g' \
+    package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
