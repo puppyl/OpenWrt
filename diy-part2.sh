d5541566e7e7dc75043f067682e2c5a9b4d2977e
aaaol <28624574+aaaol@users.noreply.github.com>
2025-04-02 11:37:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,5 +16,5 @@
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 修改默认名称
+#sed -i 's/LEDE/H29K/g' package/base-files/files/bin/config_generate
