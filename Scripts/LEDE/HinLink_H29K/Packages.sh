ba2b550911837c4b9a3b1042ec37fd42db75bd86
aaaol <28624574+aaaol@users.noreply.github.com>
2025-04-07 17:12:30 +0800

    optimized code

--- Scripts/LEDE/HinLink_H29K/Packages.sh
+++ Scripts/LEDE/HinLink_H29K/Packages.sh
@@ -9,27 +9,30 @@ UPDATE_PACKAGE() {
     local PKG_LIST=("$PKG_NAME" $5)  # 第5个参数为自定义名称列表
     local REPO_NAME=${PKG_REPO#*/}
 
-    echo " "
+    echo
 
     # 删除本地可能存在的不同名称的软件包
     for NAME in "${PKG_LIST[@]}"; do
         # 查找匹配的目录
-        echo "Search directory: $NAME"
+        echo "搜索目录：$NAME"
         local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
 
         # 删除找到的目录
         if [ -n "$FOUND_DIRS" ]; then
             while read -r DIR; do
                 rm -rf "$DIR"
-                echo "Delete directory: $DIR"
+                echo "删除目录：$DIR"
             done <<< "$FOUND_DIRS"
         else
-            echo "Not fonud directory: $NAME"
+            echo "未找到目录：$NAME"
         fi
     done
 
     # 克隆 GitHub 仓库
-    git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+    git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git" || {
+        echo "克隆仓库失败：$PKG_REPO"
+        return
+    }
 
     # 处理克隆的仓库
     if [[ $PKG_SPECIAL == "pkg" ]]; then
@@ -40,18 +43,6 @@ UPDATE_PACKAGE() {
     fi
 }
 
-# 调用示例
-# UPDATE_PACKAGE "OpenAppFilter" "destan19/OpenAppFilter" "master" "" "custom_name1 custom_name2"
-# UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf" 这样会把原有的open-app-filter，luci-app-appfilter，oaf相关组件删除，不会出现coremark错误。
-
-# UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
-UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
-UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
-UPDATE_PACKAGE "qmodem" "FUjr/modem_feeds" "main"
-UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-
 # 更新软件包版本
 UPDATE_VERSION() {
     local PKG_NAME=$1
@@ -59,11 +50,11 @@ UPDATE_VERSION() {
     local PKG_FILES=$(find ./ ../feeds/packages/ -maxdepth 3 -type f -wholename "*/$PKG_NAME/Makefile")
 
     if [ -z "$PKG_FILES" ]; then
-        echo "$PKG_NAME not found!"
+        echo "$PKG_NAME 未找到!"
         return
     fi
 
-    echo -e "\n$PKG_NAME version update has started!"
+    echo -e "\n$PKG_NAME 版本更新开始！"
 
     for PKG_FILE in $PKG_FILES; do
         local PKG_REPO=$(grep -Po "PKG_SOURCE_URL:=https://.*github.com/\K[^/]+/[^/]+(?=.*)" $PKG_FILE)
@@ -80,19 +71,27 @@ UPDATE_VERSION() {
         local NEW_URL=$(echo $PKG_URL | sed "s/\$(PKG_VERSION)/$NEW_VER/g; s/\$(PKG_NAME)/$PKG_NAME/g")
         local NEW_HASH=$(curl -sL "$NEW_URL" | sha256sum | cut -d ' ' -f 1)
 
-        echo "old version: $OLD_VER $OLD_HASH"
-        echo "new version: $NEW_VER $NEW_HASH"
+        echo "旧版本：$OLD_VER $OLD_HASH"
+        echo "新版本：$NEW_VER $NEW_HASH"
 
         if [[ $NEW_VER =~ ^[0-9].* ]] && dpkg --compare-versions "$OLD_VER" lt "$NEW_VER"; then
             sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$NEW_VER/g" "$PKG_FILE"
             sed -i "s/PKG_HASH:=.*/PKG_HASH:=$NEW_HASH/g" "$PKG_FILE"
-            echo "$PKG_FILE version has been updated!"
+            echo "$PKG_FILE 版本已更新！"
         else
-            echo "$PKG_FILE version is already the latest!"
+            echo "$PKG_FILE 版本已经是最新的！"
         fi
     done
 }
 
-# UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
+# 调用 示例
+UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
+UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
+UPDATE_PACKAGE "qmodem" "FUjr/modem_feeds" "main"
+UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+
+# 更新软件包版本
 UPDATE_VERSION "sing-box"
 UPDATE_VERSION "tailscale"
