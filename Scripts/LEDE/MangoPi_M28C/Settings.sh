0ee6817898c78cd21f10305feb461562b38919d8
aaaol <28624574+aaaol@users.noreply.github.com>
2025-04-07 20:13:10 +0800

    adjust

---
+++ Scripts/LEDE/MangoPi_M28C/Settings.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# 修改默认IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改默认主机名
+sed -i "s/set system.@system\[-1\].hostname='LEDE'/set system.@system\[-1\].hostname='H29K'/" package/base-files/files/bin/config_generate
+
+# 修改 WiFi 名称（SSID）从 LEDE → H29K
+sed -i 's/set wireless.default_radio${devidx}.ssid=LEDE/set wireless.default_radio${devidx}.ssid=H29K/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改加密方式 (none → psk2)
+sed -i 's/set wireless.default_radio${devidx}.encryption=none/set wireless.default_radio${devidx}.encryption=psk2/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 在加密行后新增密码行 (确保格式对齐)
+sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a \\t\tset wireless.default_radio${devidx}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 替换时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
+if ! grep -q "zonename=" package/base-files/files/bin/config_generate; then
+    sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+else
+    sed -i "s/zonename='.*'/zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+fi
+
+# 替换ntp服务器
+sed -i 's/0.openwrt.pool.ntp.org/ntp.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/cn.ntp.org.cn/g' package/base-files/files/bin/config_generate
+
+# 默认 shell 为 bash
+sed -i 's/\/bin\/ash/\/bin\/bash/g' package/base-files/files/etc/passwd
+
+## samba设置
+# enable multi-channel
+sed -i '/workgroup/a \\n\t## enable multi-channel' feeds/packages/net/samba4/files/smb.conf.template
+sed -i '/enable multi-channel/a \\tserver multi channel support = yes' feeds/packages/net/samba4/files/smb.conf.template
+# default config
+sed -i 's/#aio read size = 0/aio read size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#aio write size = 0/aio write size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/bind interfaces only = yes/bind interfaces only = no/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#create mask/create mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#directory mask/directory mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' feeds/luci/applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/samba.config
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.template
+
+# 最大连接数修改为65535
+sed -i '$a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
+# 显示增加编译时间
+if [ "${REPO_BRANCH#*-}" = "23.05" ]; then
+   sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION=\"LEDE R$(TZ=UTC-8 date +'%y.%-m.%-d') (By @aaaol build $(TZ=UTC-8 date '+%Y-%m-%d %H:%M'))\"/g"  package/base-files/files/etc/openwrt_release
+   echo -e "\e[41m当前写入的编译时间:\e[0m \e[33m$(grep 'DISTRIB_DESCRIPTION' package/base-files/files/etc/openwrt_release)\e[0m"
+else
+   sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION=\"LEDE By @aaaol\"/g"  package/base-files/files/etc/openwrt_release
+   sed -i "s/OPENWRT_RELEASE=.*/OPENWRT_RELEASE=\"LEDE R$(TZ=UTC-8 date +'%y.%-m.%-d') (By @aaaol build $(TZ=UTC-8 date '+%Y-%m-%d %H:%M'))\"/g"  package/base-files/files/usr/lib/os-release
+   echo -e "\e[41m当前写入的编译时间:\e[0m \e[33m$(grep 'OPENWRT_RELEASE' package/base-files/files/usr/lib/os-release)\e[0m"
+fi
+
+# 固件更新地址
+sed -i '/CPU usage/a\                <tr><td width="33\%"><\%:Compile update\%></td><td><a target="_blank" href="https://github.com/aaaol/OpenWrt/releases">👇查看</a></td></tr>' package/lean/autocore/files/arm/index.htm
+
+echo "========================="
+echo " DIY2 配置完成……"
---
+++ Scripts/LEDE/X86_64/Packages.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# 安装和更新软件包
+UPDATE_PACKAGE() {
+    local PKG_NAME=$1
+    local PKG_REPO=$2
+    local PKG_BRANCH=$3
+    local PKG_SPECIAL=$4
+    local PKG_LIST=("$PKG_NAME" $5)  # 第5个参数为自定义名称列表
+    local REPO_NAME=${PKG_REPO#*/}
+
+    echo
+
+    # 删除本地可能存在的不同名称的软件包
+    for NAME in "${PKG_LIST[@]}"; do
+        # 查找匹配的目录
+        echo "搜索目录：$NAME"
+        local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
+
+        # 删除找到的目录
+        if [ -n "$FOUND_DIRS" ]; then
+            while read -r DIR; do
+                rm -rf "$DIR"
+                echo "删除目录：$DIR"
+            done <<< "$FOUND_DIRS"
+        else
+            echo "未找到目录：$NAME"
+        fi
+    done
+
+    # 克隆 GitHub 仓库
+    git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git" || {
+        echo "克隆仓库失败：$PKG_REPO"
+        return
+    }
+
+    # 处理克隆的仓库
+    if [[ $PKG_SPECIAL == "pkg" ]]; then
+        find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
+        rm -rf ./$REPO_NAME/
+    elif [[ $PKG_SPECIAL == "name" ]]; then
+        mv -f $REPO_NAME $PKG_NAME
+    fi
+}
+
+# 更新软件包版本
+UPDATE_VERSION() {
+    local PKG_NAME=$1
+    local PKG_MARK=${2:-false}
+    local PKG_FILES=$(find ./ ../feeds/packages/ -maxdepth 3 -type f -wholename "*/$PKG_NAME/Makefile")
+
+    if [ -z "$PKG_FILES" ]; then
+        echo "$PKG_NAME 未找到!"
+        return
+    fi
+
+    echo -e "\n$PKG_NAME 版本更新开始！"
+
+    for PKG_FILE in $PKG_FILES; do
+        local PKG_REPO=$(grep -Po "PKG_SOURCE_URL:=https://.*github.com/\K[^/]+/[^/]+(?=.*)" $PKG_FILE)
+        local PKG_TAG=$(curl -sL "https://api.github.com/repos/$PKG_REPO/releases" | jq -r "map(select(.prerelease == $PKG_MARK)) | first | .tag_name")
+
+        local OLD_VER=$(grep -Po "PKG_VERSION:=\K.*" "$PKG_FILE")
+        local OLD_URL=$(grep -Po "PKG_SOURCE_URL:=\K.*" "$PKG_FILE")
+        local OLD_FILE=$(grep -Po "PKG_SOURCE:=\K.*" "$PKG_FILE")
+        local OLD_HASH=$(grep -Po "PKG_HASH:=\K.*" "$PKG_FILE")
+
+        local PKG_URL=$([[ $OLD_URL == *"releases"* ]] && echo "${OLD_URL%/}/$OLD_FILE" || echo "${OLD_URL%/}")
+
+        local NEW_VER=$(echo $PKG_TAG | sed -E 's/[^0-9]+/\./g; s/^\.|\.$//g')
+        local NEW_URL=$(echo $PKG_URL | sed "s/\$(PKG_VERSION)/$NEW_VER/g; s/\$(PKG_NAME)/$PKG_NAME/g")
+        local NEW_HASH=$(curl -sL "$NEW_URL" | sha256sum | cut -d ' ' -f 1)
+
+        echo "旧版本：$OLD_VER $OLD_HASH"
+        echo "新版本：$NEW_VER $NEW_HASH"
+
+        if [[ $NEW_VER =~ ^[0-9].* ]] && dpkg --compare-versions "$OLD_VER" lt "$NEW_VER"; then
+            sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$NEW_VER/g" "$PKG_FILE"
+            sed -i "s/PKG_HASH:=.*/PKG_HASH:=$NEW_HASH/g" "$PKG_FILE"
+            echo "$PKG_FILE 版本已更新！"
+        else
+            echo "$PKG_FILE 版本已经是最新的！"
+        fi
+    done
+}
+
+# 调用 示例
+UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
+UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+# UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
+UPDATE_PACKAGE "qmodem" "FUjr/modem_feeds" "main"
+UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "adguardhome" "xiaoxiao29/luci-app-adguardhome" "master"
+# 更新软件包版本
+UPDATE_VERSION "sing-box"
+UPDATE_VERSION "tailscale"
